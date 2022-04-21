6d1af756dab1fd2b8158e275314f9a9a6a2592c1
org100h <myrror555@gmail.com>
2022-04-21 15:13:22 +0300

    shell script fix

--- Company-Project/scripts/setup_git_flow.sh
+++ Company-Project/scripts/setup_git_flow.sh
@@ -4,5 +4,6 @@ git config gitflow.branch.master 'main'
 git flow init -d -t 'v'
 
 chmod 755 ./git-hooks/bump-version.sh
-ln -nfs ./git-hooks/bump-version.sh ./.git/hooks/post-flow-release-start
-ln -nfs ./git-hooks/bump-version.sh ./.git/hooks/post-flow-hotfix-start
+cd .git/hooks
+ln -nfs ../../git-hooks/bump-version.sh post-flow-release-start
+ln -nfs ../../git-hooks/bump-version.sh post-flow-hotfix-start
