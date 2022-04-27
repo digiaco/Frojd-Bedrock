667d0b24b9d61577607d55b127fa1a5e85a96783
org100h <myrror555@gmail.com>
2022-04-27 12:08:03 +0300

    remove comment

--- {{cookiecutter.project_name}}/config/application.php
+++ {{cookiecutter.project_name}}/config/application.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Your base production configuration goes in this file. Environment-specific
- * overrides go in their respective config/environments/{{WP_ENV}}.php file.
+
  *
  * A good default policy is to deviate from the production config as little as
  * possible. Try to define as much of your configuration in this file as you
