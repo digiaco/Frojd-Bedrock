eee5366f04d4aa2a39517cc97d31ad38542de017
org100h <org100h@DESKTOP-8GULGUG.localdomain>
2022-04-27 11:52:42 +0300

    update bedrock

--- {{cookiecutter.project_name}}/src/app/themes/main/templates/partials/popup-cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $text = get_field('cookie_popup_text', 'option');
-    $text = $text ?: '--Cookie text--';
-?>
-
-<div class="popup popup--cookie js-cookie-popup js-state-initial">
-    <div class="popup__message"><?= $text ?></div>
-    <button type="button" class="popup__close js-cookie-close"><?=__('Close', 'sage') ?></button>
-</div>
