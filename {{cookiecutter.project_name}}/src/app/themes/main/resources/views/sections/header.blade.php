bc29d6f3269c92c4676a99427649f752c269b260
org100h <myrror555@gmail.com>
2022-05-05 15:11:02 +0300

    add raw for tamplete

--- {{cookiecutter.project_name}}/src/app/themes/main/resources/views/sections/header.blade.php
+++ {{cookiecutter.project_name}}/src/app/themes/main/resources/views/sections/header.blade.php
@@ -1,11 +1,13 @@
+{% raw -%}
 <header class="banner">
   <a class="brand" href="{{ home_url('/') }}">
-  {% raw -%}  {!! $siteName !!} {% endraw -%}
+  {!! $siteName !!}
   </a>
 
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-    {% raw -%}   {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!} {% endraw -%}
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!} 
     </nav>
   @endif
 </header>
+{% endraw -%}
